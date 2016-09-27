ca230ed020077d6493aed7e5986aa5c0110d6579
Chris Pierce <cpierce@csdurant.com>
2016-09-26 19:03:16 -0500

    typo

--- Controller/Component/PaypalWPPComponent.php
+++ Controller/Component/PaypalWPPComponent.php
@@ -3,21 +3,21 @@
  * Load Config File Settings
  */
 Configure::load('PaypalWPP.paypal');
- 
+
 /*
  * Paypal WPP Component
- * 
+ *
  * @author Chris Pierce <cpierce@csdurant.com>
  *
  */
 App::uses('Component', 'Controller');
 
 class PaypalWPPComponent extends Component {
-	
+
 	/*
 	 * Web Payments Pro Hash
-	 * 
-	 * @thorws BadRequestException
+	 *
+	 * @throws BadRequestException
 	 * @param string $method, string $nvp
 	 * @return mixed[mixed]
 	 */
@@ -29,33 +29,33 @@ class PaypalWPPComponent extends Component {
 		curl_setopt($curl_handler, CURLOPT_SSL_VERIFYHOST, false);
 		curl_setopt($curl_handler, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($curl_handler, CURLOPT_POST, 1);
-				
+
 		$required_nvp = 'METHOD='.$method;
 		$required_nvp .= '&VERSION='.urlencode(Configure::read('Paypal.version'));
 		$required_nvp .= '&USER='.urlencode(Configure::read('Paypal.username'));
 		$required_nvp .= '&PWD='.urlencode(Configure::read('Paypal.password'));
 		$required_nvp .= '&SIGNATURE='.urlencode(Configure::read('Paypal.signature'));
-		
+
 		curl_setopt($curl_handler, CURLOPT_POSTFIELDS, $required_nvp.$nvp);
 		$http_responder = curl_exec($curl_handler);
-		
+
 		if (!$http_responder) {
 			throw new BadRequestException($method.'failed: '.curl_error($curl_handler).' ('.curl_errno($curl_handler).')');
 		}
-		
+
 		$responder = explode('&', $http_responder);
 		$parsed_response = array();
-		
+
 		foreach($responder as $response) {
 			$response_array = explode('=', $response);
 			if (count($response_array) >= 1)
 				$parsed_response[$response_array[0]] = urldecode($response_array[1]);
 		}
-		
+
 		if ((count($parsed_response) < 1) || !array_key_exists('ACK', $parsed_response))
 			throw new BadRequestException('Invalid HTTP Response for POST request ('.$required_nvp.$nvp.') to '.Configure::read('Paypal.endpoint'));
-		
+
 		return $parsed_response;
 	}
 
-} 
+}
